e14756ed6c51a4641d8d3d228cbf1711f22fe159
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-09-01 14:48:43 +0530

    Update Bipartite Graph.cpp

--- Graph/Bipartite Graph.cpp
+++ Graph/Bipartite Graph.cpp
@@ -1,5 +1,7 @@
 https://practice.geeksforgeeks.org/problems/bipartite-graph/1
 
+BFS - 
+	
 bool bfs(int node, vector<int>adj[], vector<int>& color){
 	    queue<int>q;
 	    q.push(node);
@@ -32,6 +34,38 @@ bool bfs(int node, vector<int>adj[], vector<int>& color){
 	        if(color[i] == -1){
 	            if(bfs(i, adj, color) == false)
 	            return false;
+	        }
+	    }
+	    return true;
+	}
+
+DFS - 
+
+bool dfs(int node, vector<int>adj[], vector<int>& color, int colors){
+	    color[node] = colors;
+	    
+	    for(auto i : adj[node]){
+	        if(color[i] == -1){
+	            if(dfs(i, adj, color, !colors) == false)
+	                return false;
+	        }
+	        else{
+	            if(color[i] == color[node])
+	                return false;
+	        }
+	    }
+	    
+	    return true;
+    }
+	bool isBipartite(int n, vector<int>adj[]){
+	    // Code here
+	    
+	    vector<int>color(n, -1);
+	    
+	    for(int i = 0; i < n; i++){
+	        if(color[i] == -1){
+	            if(dfs(i, adj, color, 0) == false)
+	                return false;
 	        }
 	    }
 	    return true;
