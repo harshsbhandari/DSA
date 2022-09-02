afbc126a62844c63bf30c29077438bbced20f1fa
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-09-02 11:05:40 +0530

    Create Detect cycle in a directed graph.cpp

---
+++ Graph/Detect cycle in a directed graph.cpp
@@ -0,0 +1,39 @@
+https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+
+bool dfs(int node, vector<int> adj[],  vector<int>& vis,  vector<int>& pathvis){
+        vis[node] = 1;
+        pathvis[node] = 1;
+        
+        for(auto i : adj[node]){
+            //  if the node is not visited
+            
+            if(!vis[i]){
+                //  if we get a cycle while doing the traversal
+                if(dfs(i, adj, vis, pathvis) == true)
+                    return true;
+            }
+            /*
+                if the node has been previously visited but it has to be visited 
+                on the same path
+            */
+            else if(vis[i] && pathvis[i])
+                return true;
+        }
+        //  marking the path as unvisited while going back*
+        
+        pathvis[node] = 0;
+        return false;
+    }
+    bool isCyclic(int n, vector<int> adj[]) {
+        // code here
+        vector<int>vis(n, 0), pathvis(n, 0);
+        
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+                if(dfs(i, adj, vis, pathvis) == true)
+                    return true;
+            }
+        }
+        
+        return false;
+    }
