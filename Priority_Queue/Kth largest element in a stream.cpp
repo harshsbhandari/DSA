c84878e9586d7ab9e3c1ae6897cbd5a1a9dd6e63
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-09-04 19:55:49 +0530

    Create Kth largest element in a stream.cpp

---
+++ Priority_Queue/Kth largest element in a stream.cpp
@@ -0,0 +1,31 @@
+https://practice.geeksforgeeks.org/problems/kth-largest-element-in-a-stream2220/1?page=2&company[]=Cisco&sortBy=submissions
+
+vector<int> kthLargest(int k, int arr[], int n) {
+        // code here
+        vector<int> ans(n);
+ 
+        // Creating a min-heap using priority queue
+        priority_queue<int, vector<int>, greater<int> > pq;
+     
+        // Iterating through each element
+        for (int i = 0; i < n; i++) {
+            // If size of priority
+            // queue is less than k
+            if (pq.size() < k)
+                pq.push(arr[i]);
+            else {
+                if (arr[i] > pq.top()) {
+                    pq.pop();
+                    pq.push(arr[i]);
+                }
+            }
+     
+            // If size is less than k
+            if (pq.size() < k)
+                ans[i] = -1;
+            else
+                ans[i] = pq.top();
+        }
+     
+        return ans;
+    }
