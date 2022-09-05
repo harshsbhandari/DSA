180013111ca76f63e145d90739a2c68e5cc9517e
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-09-05 11:19:38 +0530

    Create Does array represent Heap.cpp

---
+++ Priority_Queue/Does array represent Heap.cpp
@@ -0,0 +1,16 @@
+https://practice.geeksforgeeks.org/problems/does-array-represent-heap4345/1?page=2&company[]=Cisco&sortBy=submissions
+
+bool isMaxHeap(int arr[], int n){
+        // Your code goes here
+        
+        for(int i = 0; i <= (n-2)/2; i++){
+            // If left child is greater, return false
+            if(arr[2*i +1] > arr[i])
+                return false;
+        
+            // If right child is greater, return false
+            if(2*i+2 < n && arr[2*i+2] > arr[i])
+                return false;
+        }
+        return true;
+    }
