f1d233dbdf2094a012939710666def660f3be0e7
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-10-10 15:48:22 +0530

    Create Shortest Path in Weighted undirected graph.cpp

---
+++ Graph/Shortest Path in Weighted undirected graph.cpp
@@ -0,0 +1,53 @@
+https://practice.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-path-in-weighted-undirected-graph
+
+vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        // Code here
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >pq;
+        vector<int>ans, parent(n+1), dist(n+1, INT_MAX);
+        vector<pair<int,int>>adj[n+1];
+        
+        for(int i = 1; i <= n; i++)
+            parent[i] = i;
+        
+        for(auto it : edges){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+    
+        dist[1] = 0;
+        pq.push({0, 1});
+        
+        while(!pq.empty()){
+            int dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            for(auto it : adj[node]){
+                int edgeNode = it.first;
+                int edgeWeight = it.second;
+                
+                if((dis + edgeWeight) < dist[edgeNode]){
+                    dist[edgeNode] = (dis + edgeWeight);
+                    pq.push({(dis + edgeWeight), edgeNode});
+                    parent[edgeNode] = node;
+                }
+            }
+        }
+        
+        if(dist[n] == INT_MAX){
+            ans.push_back(-1);
+            return ans;
+        }
+        
+        int node = n;
+        
+        while(node != parent[node]){
+            ans.push_back(node);
+            node = parent[node];
+        }
+        ans.push_back(1);
+
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
