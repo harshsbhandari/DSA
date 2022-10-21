eca96998189c3c49afded6d2121a2c3b8ef8beac
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-10-21 10:03:53 +0530

    Update Rotten Oranges.cpp

--- Graph/Rotten Oranges.cpp
+++ Graph/Rotten Oranges.cpp
@@ -3,9 +3,12 @@ https://practice.geeksforgeeks.org/problems/rotten-oranges2536/1
 int orangesRotting(vector<vector<int>>& arr) {
         // Code here
         int n = arr.size(), m = arr[0].size();
-        queue<pair<pair<int,int>,int>>q;
+        
+        // {{row, col}, time}
+        queue<pair<pair<int,int>,int>>q;        
         vector<vector<int>>vis(n, vector<int>(m, 0));
         
+        // the oranges which are rotten initially are put in the queue, for further computation
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(arr[i][j] == 2){
@@ -27,6 +30,7 @@ int orangesRotting(vector<vector<int>>& arr) {
             ans = max(ans, t);
             q.pop();
             
+            // searching in all four direction of the rotten orange that, if there is any unrotten orange that is unvisited
             for(int i = 0; i < 4; i++){
                 int newr = delr[i]+row;
                 int newc = delc[i]+col;
@@ -39,6 +43,7 @@ int orangesRotting(vector<vector<int>>& arr) {
             }
         }
         
+        // checking whether all the fresh oranges have rotten or not
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(arr[i][j] == 1 && vis[i][j] != 2)
