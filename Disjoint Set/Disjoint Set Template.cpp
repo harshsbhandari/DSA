7a00d8aa06cbad2824cdcb0a2576638a7dd9367f
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-10-27 10:18:09 +0530

    Update Disjoint Set Template.cpp

--- Disjoint Set/Disjoint Set Template.cpp
+++ Disjoint Set/Disjoint Set Template.cpp
@@ -1,13 +1,16 @@
 class DisjointSet{
-    vector<int>rank, parent;
+    vector<int>rank, parent, size;
     
     public :
         DisjointSet(int n){
             rank.resize(n+1, 0);
             parent.resize(n+1);
+            size.resize(n+1);
             
-            for(int i = 0; i <= n; i++)
+            for(int i = 0; i <= n; i++){
                 parent[i] = i;
+                size[i] = 1;
+            }
         }
         
         int findParent(int node){
@@ -34,6 +37,26 @@ class DisjointSet{
             else{
                 parent[ultiV] = ultiU;
                 rank[ultiU]++;
+            }
+            
+        }
+        
+        void unionBySize(int u, int v){
+            int ultiU = findParent(u);
+            int ultiV = findParent(v);
+            
+            // Same component
+            if(ultiU == ultiV)
+                return ;
+            
+            if(size[ultiU] < size[ultiV]){
+                parent[ultiU] = ultiV;
+                size[ultiV] += size[ultiU];
+            }
+            
+            else{
+                parent[ultiV] = ultiU;
+                size[ultiU] += size[ultiV];
             }
             
         }
