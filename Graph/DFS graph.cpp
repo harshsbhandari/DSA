c6859c3cd1173365d2d84a27e16462daf75a0ef1
harshsbhandari <73375087+harshsbhandari@users.noreply.github.com>
2022-10-31 15:05:27 +0530

    Update DFS graph.cpp

--- Graph/DFS graph.cpp
+++ Graph/DFS graph.cpp
@@ -1,12 +1,12 @@
 https://practice.geeksforgeeks.org/batch-problems/depth-first-traversal-for-a-graph/0/?track=DSASP-Graph&batchId=154
 
-void dfs(int source, vector<int> adj[], vector<int>&v, int vis[]){
-        v.push_back(source);
+void dfs(int source, vector<int> adj[], vector<int>&ans, int vis[]){
+        ans.push_back(source);
         vis[source] = 1;
         
         for(auto i : adj[source]){
             if(!vis[i])
-                dfs(i, adj, v, vis);
+                dfs(i, adj, ans, vis);
         }
         
     }
@@ -14,9 +14,9 @@ void dfs(int source, vector<int> adj[], vector<int>&v, int vis[]){
     vector<int> dfsOfGraph(int n, vector<int> adj[]) {
         int vis[n] = {0};
         int source = 0;
-        vector<int>v;
+        vector<int>ans;
         
-        dfs(source, adj, v, vis);
+        dfs(source, adj, ans, vis);
     
-        return v;
+        return ans;
     }
